2df4112aa5f4a412f39a9f1b54681f415dbacec4
Aoko <hejunqin233@gmail.com>
2019-09-03 15:18:18 +0800

    refactor:优化

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { createAction, ActionType } from "./createAction";
+export { createAction, ActionType, ActionCreator } from "./createAction";
 export { createReducer } from "./createReducer";
 export { ReducerCreator, ReducerHandeler } from "./reducerCreator";
